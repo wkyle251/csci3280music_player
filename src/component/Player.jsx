ea96c052690c47b8bc6dc71a1c074c3152124bb9
wkyle251 <1155161938@link.cuhk.edu>
2023-05-03 06:49:52 +0800

    update

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,36 +1,91 @@
-import React, { useEffect, useState, useMemo } from "react"
-import db from "../../assets/db"
-import Table2 from "./Table"
-import Bar from "./Bar"
+import React, { useEffect, useState, useMemo } from 'react'
+import db from '../../assets/db'
+import Table2 from './Table'
+import Bar from './Bar'
+import { Button } from '@mui/material'
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+const Player = ({}, context) => {
+  const [list, setlist] = useState(db)
+  const [song, setsong] = useState('')
+  const [play, setplay] = useState(false)
 
-const Player = ({ }, context) => {
+  const [fileData, setFileData] = useState(null)
+  const [sckdata,setdata] = useState(null)
+  const [sckmsg,setmsg] = useState(null)
+  const [ready,setready] = useState(false)
 
-    const [list, setlist] = useState(db)
-    const [song, setsong] = useState("")
-    const [play, setplay] = useState(false)
+  const socket = new WebSocket('ws://192.168.50.239:8765')
+  socket.onopen = () => {
+    console.log('open connection')
+    // setready(true)
+  }
+  function b2 (blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        const arrayBuffer = reader.result
+        const decoder = new TextDecoder('utf-8')
+        const jsonStr = decoder.decode(arrayBuffer)
+        const dictList = JSON.parse(jsonStr)
+        resolve(dictList)
+      }
+      reader.onerror = () => {
+        reject(reader.error)
+      }
+      reader.readAsArrayBuffer(blob)
+    })
+  }
+  // Connection opened
+  // Connection opened
+  socket.addEventListener('open', event => {
+    socket.send('Hello Server!')
+    // socket.send('getSong:')
+    // setready(true)
+  })
+  socket.onmessage = event => {
+    const data = event.data
+    setdata(data)
+    // console.log(
+    //   b2(data).then(res => {
+    //     console.log(res)
+    //   })
+    // )
+  }
+  // Listen for messages
+  socket.addEventListener('message', event => {
+    console.log('Message from server ', event.data)
+  })
+  const sock = (msg) => {
+    // let msg = `checkAva:${JSON.stringify(list[2])}`
+    console.log("send:",msg)
+    socket.send(msg)
+  }
 
-
-
-    const [fileData, setFileData] = useState(null);
-
-    return (
-        <div>
-
-
-            <Table2 songback={(song) => setsong(song)} />
-            {/* {list.map()} */}
-            the player
-            get folder dest:
-            <input type="file" id="dirInput" webkitdirectory="true" directory="true" multiple />
-            <input type="file" id="dir2" />
-            <Bar
-                song={song}
-            // play={play}
-            // databack={(val) => setplay(val)}
-            />
-        </div>
-    )
-
-
+  return (
+    <div>
+      <Table2 songback={song => setsong(song)}
+      sock={(msg)=>sock(msg)}
+      data={sckdata}
+      ready={ready}
+       />
+      {/* {list.map()} */}
+      the player get folder dest:
+      <input
+        type='file'
+        id='dirInput'
+        webkitdirectory='true'
+        directory='true'
+        multiple
+      />
+      <input type='file' id='dir2' />
+      <Bar
+        song={song}
+        // play={play}
+        // databack={(val) => setplay(val)}
+      />
+      <Button onClick={() => sock()}>test</Button>
+    </div>
+  )
 }
 export default Player
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,88 +1,186 @@
-import React, { useEffect, useState, useMemo } from "react"
-import db from "../../assets/db"
+import React, { useEffect, useState, useMemo } from 'react'
+import db from '../../assets/db'
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
-const Table2 = ({ songback }, context) => {
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'
+import IconButton from '@mui/material/IconButton'
+import TextField from '@mui/material/TextField'
+const Table2 = ({ songback, sock, data, ready }, context) => {
+  const [list, setlist] = useState(db)
 
+  const [searchby, setsearch] = useState('')
+  const [color,setcolor] = useState([])
 
-    const [list, setlist] = useState(db)
+  function b2 (blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        const arrayBuffer = reader.result
+        const decoder = new TextDecoder('utf-8')
+        const jsonStr = decoder.decode(arrayBuffer)
+        const dictList = JSON.parse(jsonStr)
+        resolve(dictList)
+      }
+      reader.onerror = () => {
+        reject(reader.error)
+      }
+      reader.readAsArrayBuffer(blob)
+    })
+  }
+  // if (ready){
+  //   sock()
+  // }
+  // socket.onmessage = event => {
+  //   const data = event.data
+  //   if (typeof(data) == 'string') {
 
-    const [searchby, setsearch] = useState("")
+  //   }else{
 
-    useEffect(() => {
-        if (searchby) {
-            setlist(list.filter((row) => {
-                for (const key in row) {
-                    if (row[key].toLowerCase().includes(searchby.toLowerCase()))
-                        return true;
-                }
-                return false;
-            }))
-        } else
-            setlist(db)
+  //   }
+  //   console.log(
+  //     b2(data).then(res => {
+  //       console.log(res)
+  //     })
+  //   )
+  // }
+  // Listen for messages
+  // socket.addEventListener('message', event => {
+  //   console.log('Message from server ', event.data)
+  // })
+  // const sock = () => {
+  //   let msg = `checkAva:${JSON.stringify(list[2])}`
+  //   console.log(msg, list)
+  //   socket(msg)
+  // }
 
-    }, [searchby])
+  const checkexist = (row)=>{
 
-    return (
-        <div>
-            <TextField id="standard-basic"
-                label="Search"
-                variant="filled"
-                value={searchby}
-                onChange={(val) => setsearch(val.target.value)}
-                fullWidth minWidth={650}
-            />
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Filename</TableCell>
-                            <TableCell align="right">duration</TableCell>
-                            <TableCell align="right">title</TableCell>
-                            <TableCell align="right">artist</TableCell>
-                            <TableCell align="right">album</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {list.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.time}</TableCell>
-                                <TableCell align="right">{row.title}</TableCell>
-                                <TableCell align="right">{row.artist}</TableCell>
-                                <TableCell align="right">{row.album}</TableCell>
-                                <TableCell align="right">
-                                    <IconButton aria-label="delete" size="small" onClick={() => {
+    //  db
+     const exists = db.some(obj =>
+      obj.time === row.time &&
+      obj.name === row.name &&
+      obj.title === row.title &&
+      obj.artist === row.artist &&
+      obj.album === row.album
+    );
+    return exists
+  }
 
-                                        console.log(row.name)
-                                        songback(row)
-                                    }}>
-                                        <PlayCircleIcon fontSize="inherit" />
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    )
+  function red(list) {
+    const seenEntries = new Set();
+    return list.filter(obj => {
+      const entry = obj.time + obj.name + obj.title + obj.artist + obj.album;
+      if (seenEntries.has(entry)) {
+        return false;
+      } else {
+        seenEntries.add(entry);
+        return true;
+      }
+    });
+  }
 
+  // useEffect(()=>{
+  //   sock("getSong:")
+  // },[])
 
+  useEffect(() => {
+    if (!data) return
+    if (typeof data == 'string') {
+    } else {
+      let temp = b2(data).then(res=>{
+        console.log("res",res)
+        let newlist =[...list.filter(row => {
+            for (const key in row) {
+              if (row[key].toLowerCase().includes(searchby.toLowerCase()))
+                return true
+            }
+            return false
+          }),...res]
+          newlist = red(newlist)
+          setlist(newlist)
+
+      })
+
+    }
+  }, [data])
+
+  useEffect(() => {
+    // if (searchby) {
+      sock(`getSong:${searchby}`)
+      // setlist(
+      //   list.filter(row => {
+      //     for (const key in row) {
+      //       if (row[key].toLowerCase().includes(searchby.toLowerCase()))
+      //         return true
+      //     }
+      //     return false
+      //   })
+      // )
+    // } else setlist(db)
+  }, [searchby])
+
+  return (
+    <div>
+      <TextField
+        id='standard-basic'
+        label='Search'
+        variant='filled'
+        value={searchby}
+        onChange={val => setsearch(val.target.value)}
+        fullWidth
+        minWidth={650}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+            <TableCell align='right'>location</TableCell>
+              <TableCell>Filename</TableCell>
+              <TableCell align='right'>duration</TableCell>
+              <TableCell align='right'>title</TableCell>
+              <TableCell align='right'>artist</TableCell>
+              <TableCell align='right'>album</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {list.map(row => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align='right'>{checkexist(row)?"local":"remote"}</TableCell>
+                <TableCell component='th' scope='row'>
+                  {row.name}{checkexist(row)?"  o":""}
+                </TableCell>
+                <TableCell align='right'>{row.time}</TableCell>
+                <TableCell align='right'>{row.title}</TableCell>
+                <TableCell align='right'>{row.artist}</TableCell>
+                <TableCell align='right'>{row.album}</TableCell>
+                <TableCell align='right'>
+                  <IconButton
+                    aria-label='delete'
+                    size='small'
+                    onClick={() => {
+                      console.log(row.name)
+                      songback(row)
+                    }}
+                  >
+                    <PlayCircleIcon fontSize='inherit' />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  )
 }
 export default Table2
